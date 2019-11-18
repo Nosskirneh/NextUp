dffe939a25acc7fe7d0be0f1189c9fcb67e2f2e3
Andreas Henriksson <andreaskhenriksson@gmail.com>
2019-11-18 10:57:11 +0100

    Better ColorFlow detection

--- Headers.h
+++ Headers.h
@@ -180,6 +180,17 @@ typedef enum UIImpactFeedbackStyle : NSInteger {
 - (void)cfw_colorize:(CFWColorInfo *)colorInfo;
 - (void)cfw_revert;
 @end
+
+@interface CFWPrefsManager : NSObject
+@property(nonatomic, assign, getter=isLockScreenEnabled) BOOL lockScreenEnabled;
+@property(nonatomic, assign, getter=isMusicEnabled) BOOL musicEnabled;
+@property(nonatomic, assign, getter=isSpotifyEnabled) BOOL spotifyEnabled;
+
+@property(nonatomic, assign, getter=shouldRemoveArtworkShadow) BOOL removeArtworkShadow;
+@property(nonatomic, assign, getter=isLockScreenResizingEnabled) BOOL lockScreenResizingEnabled;
+
++ (instancetype)sharedInstance;
+@end
 // ---
 
 /* Nereid */
